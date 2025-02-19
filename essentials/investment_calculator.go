b4ba454dbb77f2cb54bcb70d61a6e03ec7bc95d7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-19 18:42:45 -0500

    Return future values from calculateFutureValues function
    - Enable the function to return both future value and
      real future value after inflation adjustment.

--- essentials/investment_calculator.go
+++ essentials/investment_calculator.go
@@ -38,6 +38,6 @@ func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) 
 	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	frv = fv / math.Pow(1+inflationRate/100, years)
 
-	// return fv, frv
-	return
+	// return
+	return fv, frv
 }
