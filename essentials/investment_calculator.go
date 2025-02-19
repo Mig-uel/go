3a13451bf50c2a003e6fda897cd1fe7c81b6b9a1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-18 22:05:42 -0500

    Refactor investment calculator for clarity and accuracy
    - Separate variable declarations for investmentAmount and years
    - Correct future value calculation to use proper formula

--- essentials/investment_calculator.go
+++ essentials/investment_calculator.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount, years = 1000, 10
+	var investmentAmount = 1000
+	var years = 10
 	var expectedReturnRate = 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow((1 + expectedReturnRate/float64(years)), expectedReturnRate) 
+	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate/100, float64(years)) 
 
 	fmt.Println(futureValue) // Println - Println adds a new line at the end of the output
 }
