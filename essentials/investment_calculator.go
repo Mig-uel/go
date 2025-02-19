7a4869fe85f57bbf44383b2c5813df1c6956ef54
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-18 22:15:06 -0500

    Explicitly declare variable types in investment calculator
    - Improved clarity by specifying data types for investmentAmount
      and years variables.
    - Ensured consistent type usage in future value calculation.

--- essentials/investment_calculator.go
+++ essentials/investment_calculator.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var years = 10
+	// explicit declaration of variables
+	// var <variable_name> <data_type> = <value>
+	var investmentAmount float64 = 1000
+	var years float64 = 10
 	var expectedReturnRate = 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate/100, float64(years)) 
+	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate/100, years) 
 
 	fmt.Println(futureValue) // Println - Println adds a new line at the end of the output
 }
