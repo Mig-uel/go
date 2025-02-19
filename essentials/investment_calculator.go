274717729185add6ed7173921b1ca3099117c1af
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-18 22:41:18 -0500

    Refactor variable declarations in investment calculator
    - Use type inference for expectedReturnRate
    - Group variable declarations for clarity
    - Maintain explicit types for investmentAmount and years

--- essentials/investment_calculator.go
+++ essentials/investment_calculator.go
@@ -8,11 +8,21 @@ import (
 func main() {
 	// explicit declaration of variables
 	// var <variable_name> <data_type> = <value>
-	var investmentAmount float64 = 1000
-	var years float64 = 10
-	var expectedReturnRate = 5.5
 
-	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate/100, years) 
+	// var investmentAmount, years, expectedReturnRate float64 = 1000, 10, 5.5
+
+	// var blocks can be used to group variables of the same type
+	// var (
+	// 	investmentAmount float64 = 1000
+	// 	years int = 10
+	// 	expectedReturnRate float64 = 5.5
+	// )
+
+	var investmentAmount, years float64 = 1000, 10
+	expectedReturnRate := 5.5 // type inference
+
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years) 
 
 	fmt.Println(futureValue) // Println - Println adds a new line at the end of the output
 }
