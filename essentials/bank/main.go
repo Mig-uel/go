b4fef2a5c2a6139e9b2cd90411f00b70d2ef1053
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-19 20:28:25 -0500

    Refactor main.go for improved user interaction flow
    - Introduce a loop for continuous user input until exit
    - Enhance deposit and withdrawal validation messages
    - Add a closing message to thank users for using the service

--- essentials/bank/main.go
+++ essentials/bank/main.go
@@ -7,47 +7,53 @@ func main() {
 	var balance float64 = 1000
 
 	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
 
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
+	for {
+		fmt.Println("What do you want to do?")
 
-	fmt.Print("Your choice: ")
-	fmt.Scan(&userInput)
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit Money")
+		fmt.Println("3. Withdraw Money")
+		fmt.Println("4. Exit")
 
-	if userInput == 1 {
-		fmt.Printf("Your balance is: $%.2f", balance)
-	} else if userInput == 2 {
-		var depositAmount float64
+		fmt.Print("Your choice: ")
+		fmt.Scan(&userInput)
 
-		fmt.Print("Deposit amount: $")
-		fmt.Scan(&depositAmount)
+		if userInput == 1 {
+			fmt.Printf("Your balance is: $%.2f", balance)
+		} else if userInput == 2 {
+			var depositAmount float64
 
-		if depositAmount < 1 {
-			fmt.Println("Invalid deposit amount!")
-			return
+			fmt.Print("Deposit amount: $")
+			fmt.Scan(&depositAmount)
+
+			if depositAmount < 1 {
+				fmt.Println("Invalid deposit amount!")
+				continue
+			}
+
+			balance += depositAmount
+
+			fmt.Printf("Your new balance is: $%.2f", balance)
+		} else if userInput == 3 {
+			var withdrawAmount float64
+
+			fmt.Print("Withdraw Amount: $")
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount > balance || withdrawAmount < 1 {
+				fmt.Println("Invalid withdraw amount")
+				continue
+			}
+
+			balance -= withdrawAmount
+
+			fmt.Printf("Your new balance is: $%.2f", balance)
+		} else {
+			fmt.Println("Goodbye!")
+			break
 		}
-
-		balance += depositAmount
-
-		fmt.Printf("Your new balance is: $%.2f", balance)
-	} else if userInput == 3 {
-		var withdrawAmount float64
-
-		fmt.Print("Withdraw Amount: $")
-		fmt.Scan(&withdrawAmount)
-
-		if withdrawAmount > balance || withdrawAmount < 1 {
-			fmt.Println("Invalid withdraw amount")
-			return
-		}
-
-		balance -= withdrawAmount
-
-		fmt.Printf("Your new balance is: $%.2f", balance)
-	} else {
-		fmt.Println("Goodbye!")
 	}
+
+	fmt.Println("Thanks for choosing Go Bank!")
 }
