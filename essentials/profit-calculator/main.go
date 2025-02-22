53f885f6ef1ce5d9d60639dac037e8c7b4ce2ba6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-22 04:53:51 -0500

    Add input validation and result storage in profit calculator
    - Implemented validation for revenue, expenses, and tax rate inputs
    - Added functionality to store calculated results in a text file

--- essentials/profit-calculator/main.go
+++ essentials/profit-calculator/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var revenue, expenses, taxRate float64
@@ -9,12 +13,24 @@ func main() {
 
 	// revenue
 	printAndPass("Enter the revenue: ", &revenue)
+	err := validate(revenue)
+	if err != nil {
+		panic(err)
+	}
 
 	// expenses
 	printAndPass("Enter total of expenses: ", &expenses)
+	err = validate(expenses)
+	if err != nil {
+		panic(err)
+	}
 
 	// tax rate
 	printAndPass("Enter tax rate: ", &taxRate)
+	err = validate(expenses)
+	if err != nil {
+		panic(err)
+	}
 
 	// calculate
 	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
@@ -24,15 +40,36 @@ func main() {
 	fmt.Printf("Ratio: %.3f", ratio)
 }
 
+func validate(input float64) error {
+	if input <= 0 {
+		return errors.New("input cannot be less than or equal to 0")
+	}
+
+	return nil
+}
+
 func printAndPass(text string, x *float64) {
 	fmt.Print(text)
 	fmt.Scan(x)
 }
 
 func calculate(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
+
 	ebt = revenue - expenses
 	profit = ebt - (ebt * (taxRate / 100))
 	ratio = ebt / profit
 
+	store(ebt, profit, ratio)
+
 	return ebt, profit, ratio
+}
+
+func store(ebt, profit, ratio float64) {
+	s := fmt.Sprintf("EBT: %.2f PROFIT: %.2f RATIO: %.3f", ebt, profit, ratio)
+
+	err := os.WriteFile("results.txt", []byte(s), 0664)
+
+	if err != nil {
+		panic(err)
+	}
 }
