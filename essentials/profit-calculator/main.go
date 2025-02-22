21fae4ec5bb5f2953103a81d06e6d5602d104249
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-22 05:00:04 -0500

    Refactor input validation in profit calculator
    - Move input validation directly into printAndPass function
    - Remove separate validate function for simplicity
    - Ensure inputs are greater than zero to prevent errors

--- essentials/profit-calculator/main.go
+++ essentials/profit-calculator/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 )
@@ -13,24 +12,12 @@ func main() {
 
 	// revenue
 	printAndPass("Enter the revenue: ", &revenue)
-	err := validate(revenue)
-	if err != nil {
-		panic(err)
-	}
 
 	// expenses
 	printAndPass("Enter total of expenses: ", &expenses)
-	err = validate(expenses)
-	if err != nil {
-		panic(err)
-	}
 
 	// tax rate
 	printAndPass("Enter tax rate: ", &taxRate)
-	err = validate(expenses)
-	if err != nil {
-		panic(err)
-	}
 
 	// calculate
 	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
@@ -40,17 +27,14 @@ func main() {
 	fmt.Printf("Ratio: %.3f", ratio)
 }
 
-func validate(input float64) error {
-	if input <= 0 {
-		return errors.New("input cannot be less than or equal to 0")
-	}
-
-	return nil
-}
-
 func printAndPass(text string, x *float64) {
 	fmt.Print(text)
 	fmt.Scan(x)
+
+	// validation
+	if *x <= 0 {
+		panic("input cannot be less than or equal to 0")
+	}
 }
 
 func calculate(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
